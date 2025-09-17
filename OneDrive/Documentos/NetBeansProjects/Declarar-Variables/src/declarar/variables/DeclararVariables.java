0176bcbbac8d53325364e65cb5a9b78e99298164
Camila <rissicamila07@gmail.com>
2025-09-17 19:31:01 -0300

    Actividad3

---
+++ OneDrive/Documentos/NetBeansProjects/Declarar-Variables/src/declarar/variables/DeclararVariables.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package declarar.variables;
+
+/**
+ *
+ * @author camil
+ */
+public class DeclararVariables {
+    
+    public static void main(String[] args) {
+        String nombre = "Camila";
+        int edad = 18;
+        double altura = 1.61;
+        boolean estudiante = true;
+
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Edad: " + edad);
+        System.out.println("Altura: " + altura);
+        System.out.println("Estudiante: " + estudiante);
+    }
+}
+
