7430fd8e45f03fc79665143fdf01bc07333214ea
Camila <rissicamila07@gmail.com>
2025-09-17 20:36:42 -0300

    Actividdad Unidad 1

---
+++ OneDrive/Documentos/NetBeansProjects/Mensaje-consola/src/mensaje/consola/MensajeConsola.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package mensaje.consola;
+
+public class MensajeConsola {
+
+    public static void main(String[] args) {
+        System.out.println("Nombre: Juan Pérez\nEdad: 30 años\nDirección: \"Calle Falsa 123\"");
+        
+    }
+    
+}
