6279ea85f07092afa3950292e358483b86fb43e4
Camila <rissicamila07@gmail.com>
2025-09-17 21:12:10 -0300

    Actividdad 9 Correcion Codigo

---
+++ OneDrive/Documentos/NetBeansProjects/Correccion-codigo/src/correccion/codigo/correccioncodigo.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package correccion.codigo; 
+import java.util.Scanner;
+public class correccioncodigo {
+
+    public static void main(String[] args) {
+         Scanner scanner = new Scanner(System.in);
+            
+        System.out.print("Ingresa tu nombre: ");
+        String nombre = scanner.nextLine(); // CORREGIDO
+        
+        System.out.println("Hola, " + nombre);
+
+}
+}
